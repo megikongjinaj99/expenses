5c42a31ef9b4dd02e7a93825e6bae7e57f6f54e7
megigranit <megikongjinaj99@gmail.com>
2023-09-12 09:48:03 +0200

    ExpenseFilter

--- expenses/src/components/Expenses/Expense.js
+++ expenses/src/components/Expenses/Expense.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ExpenseItem from "./ExpenseItem";
-import './Expense.css';
-import Card from '../UI/Card';
+import React, {useState} from 'react';
+import './Expense.css'
+import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
-export default function Expense(props) {
-    return (
-        <Card className='expenses'>
-            <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
-            <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
-            <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}/>
-            <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}/>
-        </Card>
-            );
+
+function Expense(props) {
+    const [filteredYear, setFilteredYear]= useState('2020');
+
+    const filterChangeHandler=(selectedYear)=>{
+        setFilteredYear(selectedYear);
+    }
+
+    const filteredExpenses = props.items.filter(expense =>{
+        return expense.date.getFullYear().toString()===filteredYear
+    })
+
+    return(
+        <div>
+            <Card className="expenses">
+                <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpensesList items={filteredExpenses}/>
+            </Card>
+        </div>
+    );
 }
+
+export default Expense;
